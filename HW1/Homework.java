1de1b0e7d39d5fed8945157746d3de08b77fcb07
Sarabveer Singh <me@sarabveer.me>
2020-09-13 14:33:36 -0400

    Add Homework 1

---
+++ HW1/Homework.java
@@ -0,0 +1,22 @@
+public class Homework {
+  public static void main(String[] args) {
+    // If it matches my sample exactly it gets a 0
+    // 1. Create a variable that holds a name
+    String name = "Sarabveer Singh";
+
+    // 2. Output the name to the console
+    System.out.println("Name: " + name);
+
+    // 3. Create a variable that holds an age (as a whole number)
+    int age = 19;
+
+    // 4. Output the age to the console
+    System.out.println("Age: " + age);
+
+    // 5. Create a variable that holds a decimal
+    double decimal = 9.5;
+
+    // 6. Output the variable
+    System.out.println("Decimal: " + decimal);
+  }
+}
